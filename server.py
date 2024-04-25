849b96f377ffc39d2f047a217b63b4a9d1a1ec3c
Basith <iambasith123@gmail.com>
2024-04-25 19:17:58 +0530

    Setup Firebase admin

--- server.py
+++ server.py
@@ -1,6 +1,11 @@
 from flask import Flask
+import firebase_admin
+from firebase_admin import credentials
+
 
 flask_app = Flask(__name__)
+cred = credentials.Certificate("food-lens-firebase.json")
+firebase_admin.initialize_app(cred)
 
 
 @flask_app.route('/')
